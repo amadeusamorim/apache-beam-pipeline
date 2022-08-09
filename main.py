30332b7ebb7d664dded9347f8ca5052829bc4023
amadeusamorim <amorim.amadeus@gmail.com>
2022-08-09 12:53:01 -0300

    Lista para dicionário

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ from apache_beam.options.pipeline_options import PipelineOptions
 pipeline_options = PipelineOptions(argv=None) # Opcoes de pipeline
 pipeline = beam.Pipeline(options=pipeline_options)
 
+colunas_dengue =[
+                'id',
+                'data_iniSE',
+                'casos',
+                'ibge_code',
+                'cidade',
+                'uf',
+                'cep',
+                'latitude',
+                'longitude']
+
+def lista_para_dicionario(elemento, colunas):
+    """
+    Recebe 2 listas
+    Retorna 1 dicionário
+    """
+    return dict(zip(colunas, elemento))
+
 def texto_para_lista(elemento, delimitador='|'): # Delimitador padrao é o pipe
     """
     Recebe um texto e um delimitador
@@ -18,6 +36,7 @@ dengue = (
     | "Leitura do dataset de dengue" >> 
         ReadFromText('casos_dengue.txt', skip_header_lines=1)
     | "De texto para lista" >> beam.Map(texto_para_lista) # Passo o metodo que retorna o elemento
+    | "De lista para dicionário" >> beam.Map(lista_para_dicionario, colunas_dengue)
     | "Mostrar resultados" >> beam.Map(print)
 ) # Nome do processo e metodo, skippando uma linha do header, retorna lista e aplica um print
 
